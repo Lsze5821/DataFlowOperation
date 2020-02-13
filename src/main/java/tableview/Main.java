cd0de404abb6be5267d5a0d21c5dd2799c8fe01c
Lsze5821 <46796206+Lsze5821@users.noreply.github.com>
2020-02-13 09:55:34 -0500

    update1

--- src/main/java/tableview/Main.java
+++ src/main/java/tableview/Main.java
@@ -4,10 +4,12 @@ import com.google.gson.stream.JsonReader;
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLException;
+import org.sqlite.core.DB;
 
 public class  Main {
 
-  public static void main(String[] args) throws IOException, CsvValidationException {
+  public static void main(String[] args) throws IOException, CsvValidationException, SQLException {
     // Literally just calls our parser right now (....and is called for tests)
     CsvParser csvP = new CsvParser("src/Data/bookstore_report2.csv");
     csvP.printCsv();
@@ -22,10 +24,16 @@ public class  Main {
     Gson gson = new Gson();
     JsonReader jread = new JsonReader(new FileReader("src/Data/authors.json"));
     AuthorParser[] authors = gson.fromJson(jread, AuthorParser[].class);
-
+    DBManager db = new DBManager();
     for (var element : authors) {
+      String name = element.getName();
+      String email = element.getEmail();
+      String url = element.getEmail();
+
+      db.insertToAuthor(name,email,url);
       System.out.println(element.getName());
     }
+
 
   }
 }
